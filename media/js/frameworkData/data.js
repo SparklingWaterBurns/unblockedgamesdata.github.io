5d751f8b5e82e93cba44a864ab6fb97bbabccdae
SparklingWaterBurns <110859540+SparklingWaterBurns@users.noreply.github.com>
2023-03-03 19:02:35 -0600

    Create data.js

---
+++ media/js/frameworkData/data.js
@@ -0,0 +1,8 @@
+//data.js
+const information = [
+    {   version : 0.2,
+        versionString : "0.2.0-jquery_supported_BETA",
+        builddate : "3-3-23",
+        esignature : "CTAYL_03a3a03d9",
+    }
+]
